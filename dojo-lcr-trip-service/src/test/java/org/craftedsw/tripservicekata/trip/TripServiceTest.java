d41da0317b49aa6f8147f087a6eccdc4fd129cd2
antoine.vaisset <antoine.vaisset.pro@gmail.com>
2019-04-04 16:08:56 +0200

    ajout des méthodes de test de base au Dojo trip service

--- dojo-lcr-trip-service/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ dojo-lcr-trip-service/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,23 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
 public class TripServiceTest {
-	
+
+    @Test
+    void getTripsByUser_with_logged_user_and_is_firend_return_trips() {
+        Assertions.fail("not implemented");
+    }
+
+    @Test
+    void getTripsByUser_with_unlogged_user_throw_exception() {
+        Assertions.fail("not implemented");
+    }
+
+    @Test
+    void getTripsByUser_with_logged_user_but_not_friends_return_no_trips() {
+        Assertions.fail("not implemented");
+    }
+
 }
