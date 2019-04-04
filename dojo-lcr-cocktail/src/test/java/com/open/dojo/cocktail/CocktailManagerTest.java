9e336c4935d0f66795c2df6b660c5c95388fe3e4
antoine.vaisset <antoine.vaisset.pro@gmail.com>
2019-04-04 12:21:52 +0200

    add gilded roses dojo

---
+++ dojo-lcr-cocktail/src/test/java/com/open/dojo/cocktail/CocktailManagerTest.java
@@ -0,0 +1,13 @@
+package com.open.dojo.cocktail;
+
+import org.junit.Test;
+
+public class CocktailManagerTest {
+
+    @Test
+    public void dummy() {
+        CocktailManager cocktailManager = new CocktailManager();
+        cocktailManager.setVisible(true);
+
+    }
+}
